e6ffb225dfbcfb0a9470f34906a2279dee04f5a7
Eduard Askadullin <askadullin@yandex.ru>
2020-01-29 12:56:46 +0500

    add start.py for external call

--- cgi-bin/start.py
+++ cgi-bin/start.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 import cgi
+import cgitb
+cgitb.enable()
 import html
-
-
 # Для корректной русской кодировки
 import sys
 import codecs
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 #
+#from urlparse import urlparse
 
-import scanMFP
+#import scanMFP
 
 form = cgi.FieldStorage()
+
 host = html.escape(form.getfirst("host", "не задано"))
 comp = html.escape(form.getfirst("comp", "не задано"))
 folder = html.escape(form.getfirst("folder", "не задано"))
-displayname = html.escape(form.getfirst("displayname", "не задано"))
+displayName = html.escape(form.getfirst("displayname", "не задано"))
 
 print("Content-type: text/html\n")
 print("""<!DOCTYPE HTML>
@@ -27,14 +29,14 @@ print("""<!DOCTYPE HTML>
         <body>""")
 
 print("<h1>Добавление сканера</h1>")
-print("<p>Host: {}</p>".format(host))
-print("<p>Компьютер: {}</p>".format(comp))
-print("<p>Папка: {}</p>".format(folder))
-print("<p>Отображаемое имя: {}</p>".format(displayname))
+print("<form action='/cgi-bin/form.py'>")
 
-folderName = '\\\\' + comp + '\\' + folder
-r = scanMFP.addScanToMFP(hostMFP = host, folderName = folderName, displayName = displayName)
-print("<div> " + r + '</div>')
+print(f"<div>МФУ: <input type='text'value = '{host}'></div>")
+print(f"<div>Компьютер:<input type='text' value='{comp}'></div>")
+print(f"<div>Папка: <input type='text' value='scan'></div>")
+print(f"<div>Имя сотрдуника: <input type = 'text' value='{displayName}'></div>")
+print('<input type="submit" value="Добавить">')
 
+print("</form>")
 print("""</body>
         </html>""")
